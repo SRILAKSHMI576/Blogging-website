f5e7dc0abeedbb7c118054ffbdd2405d7a45e89f
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-01-11 15:24:55 +0530

    Create routes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,27 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 8080;
+const mongoose = require("mongoose");
 
-app.get("/", (req, res) => res.send("Hell.....!"));
+require("dotenv").config();
 
+app.use(cors());
+app.use(express.json());
+
+const mongodb_server = process.env.SERVER_URL;
+const database_name = process.env.DATABASENAME;
+
+mongoose
+  .connect(`mongodb://${mongodb_server}/${database_name}`)
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch(err => {
+    console.error("Database connection error");
+  });
+
+const userRouter = require("./routes/User");
+
+app.use("/users", userRouter);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ backend/routes/User.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+let User = require("../models/User.model");
+
+//Create user
+router.route("/").post((req, res) => {
+  console.log("...gdfgdfg");
+});
