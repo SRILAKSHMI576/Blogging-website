233dce55fb80f97282d590dd88085c1549f0336d
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-01-11 22:10:21 +0530

    Create post method

--- backend/routes/Post.js
+++ backend/routes/Post.js
@@ -30,4 +30,23 @@ router.route("/:id").delete((req, res) => {
       return res.send(err);
     });
 });
+
+//to update post
+router.route("/:id").put((req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      post.title = req.body.title;
+      post.description = req.body.description;
+      post.likes = req.body.likes;
+      post.image_url = req.body.image_url;
+      post.author = req.body.author;
+      post.date = Date.parse(req.body.date);
+
+      post
+        .save()
+        .then(() => res.json(post))
+        .catch(err => res.status(400).json("Error: " + err));
+    })
+    .catch(err => res.status(400).json("Error: " + err));
+});
 module.exports = router;
--- backend/routes/User.js
+++ backend/routes/User.js
@@ -17,4 +17,24 @@ router.route("/").post((req, res) => {
     });
 });
 
+//to update user
+router.route("/:id").put((req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      user.email = req.body.email;
+      user.password = req.body.password;
+      user.bio = req.body.bio;
+      user.gender = req.body.gender;
+      user.age = Number(req.body.age);
+      user.first_name = req.body.first_name;
+      user.last_name = req.body.last_name;
+      user.date = Date.parse(req.body.date);
+
+      user
+        .save()
+        .then(() => res.json(user))
+        .catch(err => res.status(400).json("Error: " + err));
+    })
+    .catch(err => res.status(400).json("Error: " + err));
+});
 module.exports = router;
